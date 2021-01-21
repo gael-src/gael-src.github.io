01f80a6f685554b397a40f6f85642a95d7ea08ce
gael-src <src.gael@gmail.com>
2021-01-21 06:56:38 +0100

    added loading animation

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
 	// loaderWrapper.style.display = "none";
 	setTimeout(function () {
 		loaderWrapper.parentElement.removeChild(loaderWrapper);
-	}, 800);
+	}, 900);
 });
 
 // $(window).on("load", function () {
